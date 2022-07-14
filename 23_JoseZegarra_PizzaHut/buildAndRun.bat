69be114953ab397b36aace154a344c8a9ce2b6e4
unknown <jose.zegarra@vallegrande.edu.pe>
2022-07-13 22:06:04 -0500

    Actualizacion

--- 23_JoseZegarra_PizzaHut/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/PlantillaFreya .
-call docker rm -f PlantillaFreya
-call docker run -d -p 9080:9080 -p 9443:9443 --name PlantillaFreya com.mycompany/PlantillaFreya
